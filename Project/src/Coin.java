44207fe123c2f8179eb9dc5d3c0028e842d9e88c
olive <olive@LAPTOP-CEG2NR36>
2020-12-28 22:07:35 +0100

    game

---
+++ Project/src/Coin.java
@@ -0,0 +1,27 @@
+import java.math.*;
+
+public class Coin {
+	char[][] Feld;
+
+	public static void main(String[] args) {
+		char[][] b = { { 'a', 'b' }, { 'a', 'b' } };
+		Coin a = new Coin(b);
+		a.generateCoin();
+		for (int i = 0; i < a.Feld.length; i++) {
+			for (int j = 0; j < a.Feld[i].length; j++) {
+				System.out.println(b[i][j]);
+			}
+		}
+
+	}
+
+	public Coin(char[][] feld) {
+		// TODO Auto-generated constructor stub
+		Feld = feld;
+	}
+
+	public void generateCoin() {
+		int coinFeld = (int) (16 * Math.random());
+
+	}
+}
--- Project/src/Timer.java
+++ Project/src/Timer.java
@@ -3,18 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Timer {
-    public static void main(String[] args) throws IOException {
-        long endTime = System.currentTimeMillis() + 5000L; // 5 secs
-        while (System.currentTimeMillis() < endTime) {
-            //Enter data using BufferReader
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		long endTime = System.currentTimeMillis() + 5000L; // 5 secs
+		while (System.currentTimeMillis() < endTime) {
+			// Enter data using BufferReader
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            // Reading data using readLine
-            String name = reader.readLine();
+			// Reading data using readLine
+			String name = reader.readLine();
 
-            // Printing the read line
-            System.out.println(name);
-        }
-    }
+			// Printing the read dline
+			System.out.println(name);
+		}
+	}
+
 }
