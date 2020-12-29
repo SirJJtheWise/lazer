b814c617f51824983da914a8bf77809b5be6ed6e
DariaCappey <daria.cappey@web.de>
2020-12-29 13:52:59 +0100

    merge händisch

--- Project/src/Spielplan.java
+++ Project/src/Spielplan.java
@@ -51,6 +51,7 @@ public class Spielplan {
 			}
 			str.append("\n");
 		}
+		System.out.print("\033[H\033[2J");
 		System.out.println(str.toString());
 		str.setLength(0); // clear StringBuilder
 	}
@@ -76,6 +77,7 @@ public class Spielplan {
 			}
 			str.append("\n");
 		}
+		System.out.print("\033[H\033[2J");
 		System.out.println(str.toString());
 		str.setLength(0); // clear StringBuilder
 	}
